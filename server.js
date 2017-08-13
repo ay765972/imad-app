01794e28f72a834544829f02f0b0a1725f23ad24
ay765972 <30779679+ay765972@users.noreply.github.com>
2017-08-13 14:52:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/article1',function(req,res)
+{
+    res.send("This is article 1 ");
+});
+app.get('/article2',function(req,res)
+{
+    res.send("this is article 2");
+});
+app.get('/article2',function(req,res)
+{
+    res.send("This is article 3");
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
