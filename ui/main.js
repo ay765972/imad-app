8438480e5702eaab4258d574d0fe02733d1949b6
ay765972 <30779679+ay765972@users.noreply.github.com>
2017-08-14 20:31:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,10 @@ console.log('Loaded!');
 //change text
 var element=document.getElementById("text");
 element.innerHTML="New Html";
+
+//move image
+var img=document.getElementById("button");
+img.onclick=function()
+{
+    img.style.marginLeft='200px';
+};
